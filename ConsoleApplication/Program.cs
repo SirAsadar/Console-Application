9579677583124298a1517187c72a0ac7b9a8019c
SirAsadar <crash7863@gmail.com>
2018-03-12 21:36:09 -0400

    fixed an error: f to F

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -7,11 +7,16 @@ namespace TeleprompterConsole
     {
         static void Main(string[] args)
         {
+            var lines = ReadFrom("C:\\Users\\Asadar\\Source\\Repos\\Console-Application\\ConsoleApplication\\sampleQuotes.txt");
+            foreach(var line in lines)
+            {
+                Console.WriteLine(line);
+            }
         }
         static IEnumerable<string> ReadFrom(string file)
         {
             string line;
-            using (var reader = file.OpenText(file))
+            using (var reader = File.OpenText(file))
             {
                 while ((line = reader.ReadLine()) != null)
                 {
